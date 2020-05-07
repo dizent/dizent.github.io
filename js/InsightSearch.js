40c7684e5153c5aadc9cf853bedf06638376f833
Dizent <master@dizent.cn>
2020-05-07 16:41:34 +0800

    update blog

--- js/InsightSearch.js
+++ js/InsightSearch.js
@@ -28,6 +28,7 @@
         var $searchItems;
         if (array.length === 0) return null;
         sectionTitle = CONFIG.TRANSLATION[type];
+        console.info(JSON.stringify(array));
         switch (type) {
             case 'POSTS':
             case 'PAGES':
@@ -50,7 +51,7 @@
 
     function extractToSet (json, key) {
         var values = {};
-        var entries = json.pages.concat(json.posts);
+        var entries = json;
         entries.forEach(function (entry) {
             if (entry[key]) {
                 entry[key].forEach(function (value) {
@@ -152,7 +153,7 @@
     function search (json, keywords) {
         var WEIGHTS = weightFactory(keywords);
         var FILTERS = filterFactory(keywords);
-        var posts = json.posts;
+        var posts = json;
         // var pages = json.pages;
         var tags = extractToSet(json, 'tags');
         var categories = extractToSet(json, 'categories');
